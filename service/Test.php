a449b9023beba5ef0680c0a70c580326ae218683
dearmrli <28981944@qq.com>
2017-06-19 16:01:27 +0800

    rpc 对外提供的服务

---
+++ service/Test.php
@@ -0,0 +1,15 @@
+<?php
+/**
+ * Created by PhpStorm.
+ * User: 28981
+ * Date: 2017/6/13
+ * Time: 11:45
+ */
+class Test {
+    public function hehe() {
+        return 'function hehe';
+    }
+    public function hello($params) {
+        return json_encode($params);
+    }
+}
